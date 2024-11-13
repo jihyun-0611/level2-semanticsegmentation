725aba85b6f8bf78af29f0d74577372e41c4e3fb
jihyun-0611 <ahi0611@gmail.com>
2024-11-13 16:58:34 +0900

    :sparkles: load trained model as dict

--- inference.py
+++ inference.py
@@ -23,7 +23,7 @@ def test(model, data_loader, thr=0.5):
     with torch.no_grad():
         for step, (images, image_names) in tqdm(enumerate(data_loader), total=len(data_loader)):
             images = images.cuda()    
-            outputs = model(images)['out']
+            outputs = model(images)
             
             outputs = F.interpolate(outputs, size=(2048, 2048), mode="bilinear")
             outputs = torch.sigmoid(outputs)
