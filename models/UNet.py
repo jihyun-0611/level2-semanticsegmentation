7055767f01ea04b9348b6e0ce161fdf25ba3c2ef
Seyeon Kim <ksyrobot@gmail.com>
2024-11-14 14:04:21 +0900

    :sparkles: Add config parameters to model (feat #19)

--- models/UNet.py
+++ models/UNet.py
@@ -2,12 +2,15 @@ import segmentation_models_pytorch as smp
 from models.base_model import BaseModel
 
 class UNet(BaseModel):
-    def __init__(self):
+    def __init__(self, config):
         super(UNet, self).__init__()
-        self.model = smp.Unet(encoder_name="efficientnet-b0", 
-                              encoder_weights="imagenet", 
-                              in_channels=3, 
-                              classes=29)
+        params = config.MODEL.PARAMS
+        self.model = smp.Unet(
+            encoder_name=params.BACKBONE,
+            encoder_weights="imagenet",
+            in_channels=params.IN_CHANNELS,
+            classes=params.OUT_CHANNELS
+        )
 
     def forward(self, image):
         return self.model(image)
